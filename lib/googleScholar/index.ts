7c972a8485f95567db29a8b8c270e8e7e8d02578
Upender Kalwa <upender000@gmail.com>
2020-05-03 10:41:44 -0400

    Setup modules

---
+++ lib/googleScholar/index.ts
@@ -0,0 +1,18 @@
+export /**
+ *
+ *
+ * @param {string} query
+ * @param {boolean} [all=false]
+ */
+const search = (query: string, all: boolean=false) => {
+    console.log(query);
+}
+
+export /**
+ *
+ *
+ * @param {string} user
+ */
+const getUserProfile = (user: string) => {
+    console.log(user);
+}
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+import * as googleScholar from "./googleScholar";
+
+googleScholar.search("machine learning");
+googleScholar.getUserProfile("machine learning");
