04f865a81e06fafd9121d5aa097f5c768c5ac091
Upender Kalwa <gkphv@monsanto.com>
2020-05-05 20:14:35 -0400

    Remove log statements

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,8 @@
 import googleScholar from "./googleScholar";
 
-// googleScholar.search("machine learning");
-googleScholar.user("H18-9fkAAAAJ");
+googleScholar.search("machine learning").then((data) => console.log(data));
+googleScholar.user("H18-9fkAAAAJ").then((data) => console.log(data));
+
+export const user = googleScholar.user;
+export const search = googleScholar.search;
+export const all = googleScholar.all;
